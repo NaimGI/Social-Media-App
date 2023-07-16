99d0af3b7640921b3e65aaf663ef11994acf660e
NaimGI <naimbenrhouma55@gmail.com>
2023-07-17 00:08:05 +0200

    new

--- server/index.js
+++ server/index.js
@@ -4,12 +4,15 @@ import mongoose  from "mongoose";
 import bodyParser from "body-parser";
 import postRoutes from "./routes/post.js";
 import userRouter from "./routes/user.js";
+import dotenv from 'dotenv';
+
 const app =express();
 app.use(bodyParser.json({limit:"30mb",extended:true}));
 app.use(bodyParser.urlencoded({limit:"30mb",extended:true}));
 app.use(cors());
+dotenv.config();
 
-const CONNECTION_URL="mongodb+srv://naim:234@cluster0.gecowfn.mongodb.net/Social?retryWrites=true&w=majority";
+const CONNECTION_URL=process.env.MONGO_URL;
 const PORT=process.env.PORT || 2000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
